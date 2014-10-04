dead1e2b0463737e8ae996b00a5dccd47bfcc509
Meghana Srinivasulu <meghana080@gmail.com>
2014-10-05 03:56:34 +0530

    Adding BeanPostProcessor example to the repo

---
+++ BeanPostProcessor/src/com/example/spring/DisplayNameBeanPostProcessor.java
@@ -0,0 +1,22 @@
+package com.example.spring;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class DisplayNameBeanPostProcessor implements BeanPostProcessor{
+
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName)
+			throws BeansException {
+		System.out.println("Bean name after initialization:"+beanName);
+		return bean;
+	}
+
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName)
+			throws BeansException {
+		System.out.println("Bean name before initialization:"+beanName);
+		return bean;
+	}
+
+}
